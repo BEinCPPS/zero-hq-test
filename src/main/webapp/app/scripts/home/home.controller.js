afa6489644fabb92aa25290a914e50a76198aa01
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2017-03-08 10:42:41 +0100

    UI Home Page.

--- src/main/webapp/app/scripts/home/home.controller.js
+++ src/main/webapp/app/scripts/home/home.controller.js
@@ -1,53 +1,62 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	angular
-		.module('zerohqt.home')
-		.controller('HomeController', HomeController);
+    angular
+        .module('zerohqt.home')
+        .controller('HomeController', HomeController);
 
-	HomeController.$inject = ['menuItems', 'homeDataService', 'externalAppsService',
-		'$cordovaEmailComposer', 'websocketService', '$scope'];
+    HomeController.$inject = ['menuItems', 'homeDataService', 'externalAppsService',
+        '$cordovaEmailComposer', 'websocketService', '$scope'];
 
-	/* @ngInject */
-	function HomeController(menuItems, homeDataService, externalAppsService, $cordovaEmailComposer, websocketService, $scope) {
-		var messageList = [];
-		var vm = angular.extend(this, {
-			entries: messageList,
-			phoneNumber: homeDataService.phoneNumber,
-			getDirections: getDirections,
-			sendEmail: sendEmail,
-			openFacebookPage: openFacebookPage
-		});
+    /* @ngInject */
+    function HomeController(menuItems, homeDataService, externalAppsService, $cordovaEmailComposer, websocketService, $scope) {
+        var messageMap = {};
+        var vm = angular.extend(this, {
+            entries: messageMap,
+            phoneNumber: homeDataService.phoneNumber,
+            getDirections: getDirections,
+            sendEmail: sendEmail,
+            openFacebookPage: openFacebookPage
+        });
 
-		$scope.$on('wsMessage', function (event, message) {
-			console.log(message); // 'Broadcast!'
-			messageList.push(message);
-			$scope.$apply(); //Apply changes to the page
-		});
+        $scope.$on('wsMessage', function (event, informationBay) {
+            console.log(informationBay); // 'Broadcast!'
+            aggregateData(informationBay);
+            $scope.$apply(); //Apply changes to the page
+        });
 
-		function getDirections() {
-			externalAppsService.openMapsApp(homeDataService.officeLocation);
-		}
+        function getDirections() {
+            externalAppsService.openMapsApp(homeDataService.officeLocation);
+        }
 
-		function sendEmail() {
-			$cordovaEmailComposer.isAvailable().then(function () {
-				var email = {
-					to: homeDataService.email,
-					subject: 'Cordova Icons',
-					body: 'How are you? Nice greetings from Leipzig'
-				};
+        function sendEmail() {
+            $cordovaEmailComposer.isAvailable().then(function () {
+                var email = {
+                    to: homeDataService.email,
+                    subject: 'Cordova Icons',
+                    body: 'How are you? Nice greetings from Leipzig'
+                };
 
-				$cordovaEmailComposer.open(email);
-			});
-		}
+                $cordovaEmailComposer.open(email);
+            });
+        }
 
-		function openFacebookPage() {
-			externalAppsService.openExternalUrl(homeDataService.facebookPage);
-		}
+        function openFacebookPage() {
+            externalAppsService.openExternalUrl(homeDataService.facebookPage);
+        }
 
-		(function connectWebSocket() {
-			websocketService.connect();
-		})();
+        function aggregateData(informationBay) {
+            var stationName = informationBay.stationName;
+            var baies = messageMap[stationName] ? messageMap[stationName] : [];
+            var bayNumber = parseInt(informationBay.bayNumber);
+            bayNumber--;
+            baies[bayNumber] = informationBay; //Station baies numbers starts from 1
+            messageMap[stationName] = baies;
+        }
 
-	}
+        (function connectWebSocket() {
+            websocketService.connect();
+        })();
+
+    }
 })();
