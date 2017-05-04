ef4b4e87fa0f4e7d0638f5fd96fbb1a9597088b4
Massimiliano Alberti <Ultimomax@gmail.com>
2017-05-04 12:08:42 +0200

    New page Dashboard and (1 step of) restyling history and inbox

--- src/main/webapp/app/scripts/dashboard/dashboard.controller.js
+++ src/main/webapp/app/scripts/dashboard/dashboard.controller.js
@@ -9,6 +9,41 @@
 
     /* @ngInject */
     function DashboardController($scope) {
+    $scope.ctrl = this;
+
+  //  ctrl.add = add;
+    $scope.ctrl.data = [
+        {
+            name: "MeasureA",
+            code: 1500,
+            limitMax: 2500,
+            limitMin:  500
+        },
+        {
+            name: "MeasureB",
+            code: 3100,
+            limitMax: 3000,
+            limitMin: 2500
+        },
+        {
+            name: "MeasureC",
+            code: 200,
+            limitMax: 2500,
+            limitMin:  500
+         },
+         {
+            name: "MeasureD",
+            code: 4500,
+            limitMax: 2500,
+            limitMin:  500
+          }
+    ]
+    $scope.controlValue = function (value, valueMin, valueMax) {
+            if (value < valueMax && value > valueMin)  return 'green-button';
+            else if (value < valueMin) return 'yellow-button';
+            else if (value > valueMax) return 'red-button';
+
+        }
 
     }
 })();
