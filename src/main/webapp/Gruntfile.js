b928ea4b2926e5a5cd360c9831935a000d25e54c
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2017-05-17 16:59:46 +0200

    Minor UI improvements in mobile application.

--- src/main/webapp/Gruntfile.js
+++ src/main/webapp/Gruntfile.js
@@ -62,7 +62,7 @@ module.exports = function (grunt) {
                     ENV: {
                         name: 'development',
                         apiEndpointHost: 'localhost',
-                        apiEndpointHostMobile: 'bd024221.ngrok.io', //10.0.3.2
+                        apiEndpointHostMobile: 'b74bf1a0.ngrok.io', //10.0.3.2
                         apiEndPointPort: '8080',
                         apiEndPointPortMobile: '',
                         apiEndPointDefaultProtocol: 'http',
