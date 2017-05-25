4ebb738e8dd3b79ac3373a3b925c13020026c9d4
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2017-05-25 15:22:29 +0200

    Now the pom produces a war file in prod profile.

--- src/main/java/it/eng/zerohqt/ZeroHqTestApplication.java
+++ src/main/java/it/eng/zerohqt/ZeroHqTestApplication.java
@@ -2,13 +2,19 @@ package it.eng.zerohqt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @RequestMapping("/rest")
 @EnableScheduling
-public class ZeroHqTestApplication {
+public class ZeroHqTestApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ZeroHqTestApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ZeroHqTestApplication.class, args);
